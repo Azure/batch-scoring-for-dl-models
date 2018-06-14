a055ccbe8a8727bc98b9448909a205612aa7bd59
Ubuntu <jiata@js-ubuntu-ws.2udtv1pw42wejlwdvjg1irwqoh.cx.internal.cloudapp.net>
2018-06-14 22:11:30 +0000

    renamed some stuff

--- training_script/model_dev.py
+++ training_script/model_dev.py
@@ -1,112 +1,130 @@
-""" Neural Network.
-A 2-Hidden Layers Fully Connected Neural Network (a.k.a Multilayer Perceptron)
-implementation with TensorFlow. This example is using the MNIST database
-of handwritten digits (http://yann.lecun.com/exdb/mnist/).
-Links:
-    [MNIST Dataset](http://yann.lecun.com/exdb/mnist/).
-Author: Aymeric Damien
-Project: https://github.com/aymericdamien/TensorFlow-Examples/
-"""
-
-"""TODO
-Update to this:
-https://dev.to/kasperfred/putting-it-all-together---introduction-to-tensorflow-part-5-ein 
-"""
-
-from __future__ import print_function
-
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
 import tensorflow as tf
 
-# Parameters
-learning_rate = 0.1
-num_steps = 500
-batch_size = 128
-display_step = 100
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+# ==============
+# Variables 
+# ==============
+
 model_output_path = './models/model0'
 
-# Network Parameters
-n_hidden_1 = 256 # 1st layer number of neurons
-n_hidden_2 = 256 # 2nd layer number of neurons
-num_input = 784 # MNIST data input (img shape: 28*28)
-num_classes = 10 # MNIST total classes (0-9 digits)
 
-# tf Graph input
-X = tf.placeholder("float", [None, num_input], name='X')
-Y = tf.placeholder("float", [None, num_classes], name='Y')
+# ==============
+# Define Input 
+# ==============
 
-# Store layers weight & bias
-weights = {
-  'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
-  'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-  'out': tf.Variable(tf.random_normal([n_hidden_2, num_classes]))
-}
-biases = {
-  'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-  'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-  'out': tf.Variable(tf.random_normal([num_classes]))
-}
+with tf.name_scope('input') as scope:
+	x = tf.placeholder(tf.float32, [None, 28*28], name="input")
+	# a placeholder to hold the correct answer during training
+	labels = tf.placeholder(tf.float32, [None, 10], name="label")
+	# the probability of a neuron being kept during dropout
+	keep_prob = tf.placeholder(tf.float32, name="keep_prob")
 
 
-# Create model
-def neural_net(x):
-  # Hidden fully connected layer with 256 neurons
-  layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-  # Hidden fully connected layer with 256 neurons
-  layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-  # Output fully connected layer with a neuron for each class
-  out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
-  return out_layer
+# =================
+# Define Neural Net
+# =================
 
-# Construct model
-logits = neural_net(X)
-prediction = tf.nn.softmax(logits, name='preds')
+with tf.name_scope('model') as scope:
+	with tf.name_scope('fc1') as scope: # fc1 stands for 1st fully connected layer
+		# 1st layer goes from 784 neurons (input) to 200 in the first hidden layer
+		w1 = tf.Variable(tf.truncated_normal([28*28, 200], stddev=0.1), name="weights")
+		b1 = tf.Variable(tf.constant(0.1, shape=[200]), name="biases")
 
-# Define loss and optimizer
-loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-  logits=logits, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-train_op = optimizer.minimize(loss_op)
+		with tf.name_scope('relu_activation') as scope:
+			# softmax activation
+			a1 = tf.nn.relu(tf.matmul(x, w1) + b1)
 
-# Evaluate model
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
+		with tf.name_scope('dropout') as scope:
+			# dropout
+			drop1 = tf.nn.dropout(a1, keep_prob)
 
-# Initialize the variables (i.e. assign their default value)
+	with tf.name_scope('fc2') as scope:
+		# takes the first hidden layer of 200 neurons to 100 (second hidden layer)
+		w2 = tf.Variable(tf.truncated_normal([200, 100], stddev=0.1), name="weights")
+		b2 = tf.Variable(tf.constant(0.1, shape=[100]), name="biases")
+
+		with tf.name_scope('relu_activation') as scope:
+			# relu activation, and dropout for second hidden layer
+			a2 = tf.nn.relu(tf.matmul(drop1, w2) + b2)
+
+		with tf.name_scope('dropout') as scope:
+			drop2 = tf.nn.dropout(a2, keep_prob)
+
+	with tf.name_scope('fc3') as scope:
+		# takes the second hidden layer of 30 neurons to 10 (which is the output)
+		w3 = tf.Variable(tf.truncated_normal([100, 10], stddev=0.1), name="weights")
+		b3 = tf.Variable(tf.constant(0.1, shape=[10]), name="biases")
+
+		with tf.name_scope('logits') as scope:
+			# final layer doesn't have dropout
+			logits = tf.matmul(drop2, w3) + b3
+
+
+# =========================
+# Define Train / Evaluation
+# =========================
+
+with tf.name_scope('train') as scope:
+	with tf.name_scope('loss') as scope:
+		# loss function
+		cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits)
+	# use adam optimizer for training with a learning rate of 0.001
+	train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+
+with tf.name_scope('evaluation') as scope:
+	# evaluation
+	correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(labels,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	prediction = tf.nn.softmax(logits, name='preds')
+
+# create a summarizer that summarizes loss and accuracy
+tf.summary.scalar("Accuracy", accuracy)
+
+# add average loss summary over entire batch
+tf.summary.scalar("Loss", tf.reduce_mean(cross_entropy)) 
+
+# merge summaries
+summary_op = tf.summary.merge_all()
+
+
+# ============
+# Learning
+# ============
+
+saver = tf.train.Saver()
 init = tf.global_variables_initializer()
 
-# Start training
 with tf.Session() as sess:
+	# initialize variables
+	sess.run(init)
 
-  # Run the initializer
-  sess.run(init)
+	# initialize summarizer filewriter
+	# fw = tf.summary.FileWriter("/tmp/nn/summary", sess.graph)
 
-  saver = tf.train.Saver()
+	# train the network
+	for step in range(20000):
+		batch_xs, batch_ys = mnist.train.next_batch(100)
+		sess.run(train_step, feed_dict={x: batch_xs, labels: batch_ys, keep_prob: 0.2})
 
-  for step in range(1, num_steps+1):
-    batch_x, batch_y = mnist.train.next_batch(batch_size)
-    # Run optimization op (backprop)
-    sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
-    if step % display_step == 0 or step == 1:
-      # Calculate batch loss and accuracy
-      loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
-                                                           Y: batch_y})
-      print("Step " + str(step) + ", Minibatch Loss= " + \
-            "{:.4f}".format(loss) + ", Training Accuracy= " + \
-            "{:.3f}".format(acc))
+		if step%1000 == 0:
+			acc = sess.run(accuracy, feed_dict={
+				x: batch_xs, labels: batch_ys, keep_prob:1})
+			print("mid train accuracy:", acc, "at step:", step)
 
-  print("Optimization Finished!")
+		# if step%100 == 0:
+		# 	# compute summary using test data every 100 steps
+		# 	summary = sess.run(summary_op, feed_dict={
+		# 		x: mnist.test.images, labels: mnist.test.labels, keep_prob:1})
 
-  # saver.save(sess, './models/model0', global_step=1000)
-  path = saver.save(sess, model_output_path, global_step=1000)
-  print("Model saved in path: %s" % path)
+		# 	# add merged summaries to filewriter,
+		# 	# so they are saved to disk
+		# 	fw.add_summary(summary, step)
 
-  # Calculate accuracy for MNIST test images
-  print("Testing Accuracy:", \
-    sess.run(accuracy, feed_dict={X: mnist.test.images,
-                                  Y: mnist.test.labels}))
+	print("Final Test Accuracy:", sess.run(accuracy, feed_dict={
+		x: mnist.test.images, labels: mnist.test.labels, keep_prob:1}))
 
+	# save trained model
+	saver.save(sess, model_output_path)
 
